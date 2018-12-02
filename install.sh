4198e92621015595fd0bd25ee81ff4c4f7247b80
Erik Poupaert <erik@sankuru.biz>
2018-12-02 12:36:46 +0700

    added simple install/uninstall scripts

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------
+#      nacl-cli
+#      Written by Erik Poupaert, Cambodia
+#      (c) 2018
+#      Licensed under the LGPL
+#-------------------------------------------------------
+if ~ [ -e ./nacl-cli ] ; then
+    >&2 echo "Cannot find nacl-cli executable."
+    >&2 echo "Use build.sh to build it first."  
+    exit 1
+fi
+cp -f ./nacl-cli /usr/local/bin
+echo "nacl-cli installed in /usr/local/bin"
+
---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------
+#      nacl-cli
+#      Written by Erik Poupaert, Cambodia
+#      (c) 2018
+#      Licensed under the LGPL
+#-------------------------------------------------------
+echo "removing nacl-cli from /usr/local/bin"
+rm -f /usr/local/bin/nacl-cli
+
